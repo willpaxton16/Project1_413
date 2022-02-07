82b75f51a9ce853f34f3c6f83ef74054a4b0edd4
willpaxton16 <90337820+willpaxton16@users.noreply.github.com>
2022-02-07 16:48:34 -0700

    Add project files.

---
+++ Project1_413/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project1_413.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
