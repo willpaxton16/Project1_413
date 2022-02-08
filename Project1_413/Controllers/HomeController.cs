fd785639ebedfc032d510c7b2164364371628c5f
Buscompany21 <buscobrian1@gmail.com>
2022-02-07 23:42:05 -0700

    Cleaned up the outline

--- Project1_413/Controllers/HomeController.cs
+++ Project1_413/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Project1_413.Models;
+//using Project1_413.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
